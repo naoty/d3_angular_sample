ca6d1a8894bde72a381e6457a6fc1b98b484ce91
Naoto Kaneko <naoty.k@gmail.com>
2014-05-11 23:58:13 +0900

    Show points on line chart

--- dest/angular/directives/d3_time_series_chart.js
+++ dest/angular/directives/d3_time_series_chart.js
@@ -4,7 +4,7 @@
       restrict: "EA",
       replace: true,
       link: function(scope, elements, attrs) {
-        var data, height, line, padding, parseDate, svg, width, xAxis, xExtent, xScale, yAxis, yExtent, yScale;
+        var area, data, height, line, paddingBottom, paddingLeft, paddingRight, paddingTop, parseDate, svg, width, xAxis, xExtent, xScale, yAxis, yExtent, yScale;
         if (attrs.data === void 0) {
           return;
         }
@@ -15,28 +15,42 @@
         });
         width = attrs.width || 300;
         height = attrs.height || 200;
-        padding = attrs.padding || 40;
+        paddingTop = 10;
+        paddingRight = 20;
+        paddingBottom = 40;
+        paddingLeft = 40;
         svg = d3.select(elements[0]).append("svg").attr("width", width).attr("height", height);
         line = d3.svg.line();
         xExtent = d3.extent(data, function(d) {
           return d[0];
         });
-        xScale = d3.time.scale().domain(xExtent).range([padding, width - padding]);
+        xScale = d3.time.scale().domain(xExtent).range([paddingLeft, width - paddingRight]);
         line.x(function(d) {
           return xScale(d[0]);
         });
         yExtent = d3.extent(data, function(d) {
           return d[1];
         });
-        yScale = d3.scale.linear().domain(yExtent).range([height - padding, padding]);
+        yScale = d3.scale.linear().domain(yExtent).range([height - paddingBottom, paddingTop]);
         line.y(function(d) {
           return yScale(d[1]);
         });
         svg.append("path").datum(data).attr("d", line);
-        xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(data.length).tickFormat(d3.time.format("%m/%d"));
-        svg.append("g").attr("class", "x axis").attr("transform", "translate(0, " + (height - padding) + ")").call(xAxis);
-        yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(data.length).innerTickSize(-width).outerTickSize(0);
-        return svg.append("g").attr("class", "y axis").attr("transform", "translate(" + padding + ", 0)").call(yAxis);
+        xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(data.length).tickFormat(d3.time.format("%m/%d")).tickPadding(10);
+        svg.append("g").attr("class", "x axis").attr("transform", "translate(0, " + (height - paddingBottom) + ")").call(xAxis);
+        yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(data.length / 2).innerTickSize(-width).outerTickSize(0).tickPadding(10);
+        svg.append("g").attr("class", "y axis").attr("transform", "translate(" + paddingLeft + ", 0)").call(yAxis);
+        area = d3.svg.area().x(function(d) {
+          return xScale(d[0]);
+        }).y0(height - paddingBottom).y1(function(d) {
+          return yScale(d[1]);
+        });
+        svg.append("path").datum(data).attr("class", "area").attr("d", area);
+        return svg.selectAll("circle").data(data).enter().append("circle").attr("r", 4).attr("cx", function(d) {
+          return xScale(d[0]);
+        }).attr("cy", function(d) {
+          return yScale(d[1]);
+        });
       }
     };
   });
--- src/angular/directives/d3_time_series_chart.coffee
+++ src/angular/directives/d3_time_series_chart.coffee
@@ -10,7 +10,10 @@ window.ngApp.directive "d3TimeSeriesChart", ->
 
     width = attrs.width || 300
     height = attrs.height || 200
-    padding = attrs.padding || 40
+    paddingTop = 10
+    paddingRight = 20
+    paddingBottom = 40
+    paddingLeft = 40
 
     svg = d3.select(elements[0]).append("svg")
       .attr("width", width)
@@ -19,11 +22,11 @@ window.ngApp.directive "d3TimeSeriesChart", ->
     line = d3.svg.line()
 
     xExtent = d3.extent(data, (d) -> return d[0])
-    xScale = d3.time.scale().domain(xExtent).range([padding, width - padding])
+    xScale = d3.time.scale().domain(xExtent).range([paddingLeft, width - paddingRight])
     line.x((d) -> xScale(d[0]))
 
     yExtent = d3.extent(data, (d) -> return d[1])
-    yScale = d3.scale.linear().domain(yExtent).range([height - padding, padding])
+    yScale = d3.scale.linear().domain(yExtent).range([height - paddingBottom, paddingTop])
     line.y((d) -> yScale(d[1]))
 
     svg.append("path")
@@ -35,19 +38,38 @@ window.ngApp.directive "d3TimeSeriesChart", ->
       .orient("bottom")
       .ticks(data.length)
       .tickFormat(d3.time.format("%m/%d"))
+      .tickPadding(10)
     svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0, #{height - padding})")
+      .attr("transform", "translate(0, #{height - paddingBottom})")
       .call(xAxis)
 
     yAxis = d3.svg.axis()
       .scale(yScale)
       .orient("left")
-      .ticks(data.length)
+      .ticks(data.length / 2)
       .innerTickSize(-width)
       .outerTickSize(0)
+      .tickPadding(10)
     svg.append("g")
       .attr("class", "y axis")
-      .attr("transform", "translate(#{padding}, 0)")
+      .attr("transform", "translate(#{paddingLeft}, 0)")
       .call(yAxis)
+
+    area = d3.svg.area()
+      .x((d) -> xScale(d[0]))
+      .y0(height - paddingBottom)
+      .y1((d) -> yScale(d[1]))
+    svg.append("path")
+      .datum(data)
+      .attr("class", "area")
+      .attr("d", area)
+
+    svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("r", 4)
+      .attr("cx", (d) -> xScale(d[0]))
+      .attr("cy", (d) -> yScale(d[1]))
 
